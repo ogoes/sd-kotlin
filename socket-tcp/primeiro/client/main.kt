f1ba0b151aab6a2c80bc1c2e95a31ddce1766b00
ogoes <otavio@ogoes.dev>
2019-09-11 12:47:23 -0300

    ACÉFALO

--- socket-tcp/primeiro/client/main.kt
+++ socket-tcp/primeiro/client/main.kt
@@ -1,79 +1,66 @@
 package client
 
+import primeiro.SocketConnection
+
+import java.io.File
 import java.net.Socket
 import java.net.SocketException
 import java.util.*
 
-import java.io.File
-import java.nio.charset.Charset
-
-import java.nio.file.Files
-import java.nio.file.StandardOpenOption
-
-
-fun reloadFileDownloadProgressBar (loaded: Int, size: Int) {
+fun reloadFileDownloadProgressBar(loaded: Int, size: Int) {
   // println(" [          ] 99%")
 
+    val porcent: Int = ((loaded.toDouble() / size.toDouble()) * 100.0).toInt()
 
-  val porcent: Int = ((loaded.toDouble()/size.toDouble()) * 100.0).toInt()
+    val spaces = porcent / 10
 
-  if (porcent % 10 == 0) {
-
-    
     print("\u001B[s")
-    
+
     print(" |\u001B[47m")
-    for (i in 1..(porcent/10)) {
+    for (i in 1..(spaces)) {
       print(" ")
     }
     print("\u001B[0m")
-    for (i in 1..(10-(porcent/10))) {
+    for (i in 1..(10 - (spaces))) {
       print(" ")
     }
     print("| ")
+
     print(porcent)
     print("%")
-    
     print("\u001B[u")
-  }
-
 }
 
-fun receiveTimeDate(server: Client, type: String) {
+fun receiveTimeDate(server: SocketConnection, type: String) {
   server.sendMessage(type)
 
   var response: String = server.receiveTextMessage()
 
   println(response)
-
 }
 
-fun showFiles(server: Client) {
+fun showFiles(server: SocketConnection) {
   val filesCount: Int = server.receiveIntegerMessage()
 
   server.sendMessage("ACK")
-  println(filesCount)
 
-  var file: String = ""
+  var file: String
 
   for (i in 1..filesCount) {
     file = server.receiveTextMessage()
     server.sendMessage("ACK")
     println(file)
   }
-
 }
 
-fun receiveFile(server: Client, filename: String, localStorage: String = "./.client/") {
+fun receiveFile(server: SocketConnection, filename: String, localStorage: String = "./.client/") {
 
-  server.sendMessage("DOWN ${filename}")
-
+  server.sendMessage("DOWN $filename")
 
   val fileSize = server.receiveIntegerMessage()
 
   server.sendMessage("ACK")
 
-  
   if (fileSize > 0) {
     val file = File(localStorage + filename)
     val fileOutputStream = file.outputStream()
@@ -82,7 +69,7 @@ fun receiveFile(server: Client, filename: String, localStorage: String = "./.cli
       val byte = server.receiveBinaryMessage()
 
       reloadFileDownloadProgressBar(i, fileSize)
-      
+
       fileOutputStream.write(byte)
 
       server.sendMessage("ACK")
@@ -92,11 +79,9 @@ fun receiveFile(server: Client, filename: String, localStorage: String = "./.cli
   } else {
     println("Arquivo não existe")
   }
-
-
 }
 
-fun interation(socket: Client) {
+fun interation(socket: SocketConnection) {
 
   val timeRegex = Regex("time", RegexOption.IGNORE_CASE)
   val dateRegex = Regex("date", RegexOption.IGNORE_CASE)
@@ -117,13 +102,12 @@ fun interation(socket: Client) {
 
       socket.sendMessage("FILES")
       showFiles(socket)
-
     } else if (downRegex.containsMatchIn(message)) {
 
       val fileRegex = Regex("\\s[\\w\\d\\.]+")
 
       val file = fileRegex.find(message)!!
-      
+
       try {
         receiveFile(socket, file.value.substring(1))
       } catch (t: Throwable) {
@@ -145,7 +129,7 @@ fun main(args: Array<String>) {
     try {
       val socket = Socket(args[0], args[1].toInt())
 
-      val server = Client(0, socket)
+      val server = SocketConnection(0, socket)
 
       interation(server)
 
