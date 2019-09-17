8dcdb43a5513c98ac8a5b72704880ed36c8268fb
ogoes <otavio@ogoes.dev>
2019-09-16 23:14:09 -0300

    que merda vei, pior coisa que eu fiz na minha vida

--- socket-udp/segundo/client/main.kt
+++ socket-udp/segundo/client/main.kt
@@ -9,10 +9,20 @@
 package segundo.client
 
 import java.io.File
+import java.math.BigInteger
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import java.security.MessageDigest
 import kotlin.math.ceil
+
+/**
+ * Função para criar a hash md5
+ */
+fun String.md5(): String {
+    val md = MessageDigest.getInstance("MD5")
+    return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
+}
 
 // Primeiro DatagramPacket = 
 //  - Tamanho do arquivo (4 bytes) 
@@ -28,27 +38,24 @@ import kotlin.math.ceil
  * @param serverIP: IP do servidor de arquivos
  * @param serverPort: porta da conexão do servidor de arquivos
  */
-fun uploadFile(dgramSocket: DatagramSocket, filename: String, serverIP: InetAddress, serverPort: Int) {
+fun uploadFile(dgramSocket: DatagramSocket, filePath: String, serverIP: InetAddress, serverPort: Int) {
   try {
 
-    val pathName: String = "./.client"
-    val file = File(pathName + "/" + filename)
+    val file = File(filePath)
+
+    var last: Int = 0
+
+    for (i in 1..filePath.length) {
+
+      if (filePath.get(i - 1) == '/') last = i - 1
+    }
+    val filename: String = filePath.substring(last + 1)
 
     val firstBuffer: MutableList<Byte> = mutableListOf()
 
     val sizeFile: Int = file.length().toInt()
-    val sizeFilename = filename.length
     val filenameInBytes = filename.toByteArray(Charsets.UTF_8)
-
-    // Converte o tamanho do arquivo (int) para 4 bytes
-    val HEXport3: Int = sizeFile shr 24
-    val HEXport2: Int = (sizeFile and 0x00ff0000) shr 16
-    val HEXport1: Int = (sizeFile and 0x0000ff00) shr 8
-    val HEXport0: Int = sizeFile and 0x000000ff
-    firstBuffer.add(HEXport3.toByte())
-    firstBuffer.add(HEXport2.toByte())
-    firstBuffer.add(HEXport1.toByte())
-    firstBuffer.add(HEXport0.toByte())
+    val sizeFilename = filenameInBytes.size
 
     // Tamanho do nome do arquivo
     firstBuffer.add(sizeFilename.toByte())
@@ -58,12 +65,29 @@ fun uploadFile(dgramSocket: DatagramSocket, filename: String, serverIP: InetAddr
       firstBuffer.add(filenameInBytes.get(i - 1))
     }
 
-    val firstPkt: DatagramPacket = DatagramPacket(firstBuffer.toByteArray(), firstBuffer.size, serverIP, serverPort)
+    // Converte o tamanho do arquivo (int) para 4 bytes
+    val HEXport3: Int = sizeFile shr 24
+    val HEXport2: Int = (sizeFile and 0x00ff0000) shr 16
+    val HEXport1: Int = (sizeFile and 0x0000ff00) shr 8
+    val HEXport0: Int = sizeFile and 0x000000ff
+
+    firstBuffer.add(HEXport3.toByte())
+    firstBuffer.add(HEXport2.toByte())
+    firstBuffer.add(HEXport1.toByte())
+    firstBuffer.add(HEXport0.toByte())
+
+    val firstBytes = firstBuffer.toByteArray()
+
+    val firstPkt: DatagramPacket = DatagramPacket(firstBytes, firstBytes.size, serverIP, serverPort)
     dgramSocket.send(firstPkt)
+
+    val ackBuffer: ByteArray = ByteArray(4)
+    val dgramPacket: DatagramPacket = DatagramPacket(ackBuffer, ackBuffer.size)
+    dgramSocket.receive(dgramPacket) // ACK
 
     // ----------------------------------------------------- //
 
-    val numberOfPackets: Int = ceil((sizeFile / 1024).toDouble()).toInt()
+    val numberOfPackets: Int = ceil(sizeFile / 1024.0).toInt()
 
     val fileInputStream = file.inputStream()
     val bytes = ByteArray(1024)
@@ -73,9 +97,33 @@ fun uploadFile(dgramSocket: DatagramSocket, filename: String, serverIP: InetAddr
       val senderPacket: DatagramPacket = DatagramPacket(bytes, bytes.size, serverIP, serverPort)
       dgramSocket.send(senderPacket)
 
-      val receiverPacket: DatagramPacket = DatagramPacket(bytes, bytes.size, serverIP, serverPort)
-      dgramSocket.receive(receiverPacket)
+      dgramSocket.receive(dgramPacket)
     }
+
+    
+    // ----------------------------------------------------- //
+    
+    val fileBytes = ByteArray(sizeFile)
+    fileInputStream.read(fileBytes)
+    
+    val md5String: String = fileBytes.toString().md5()
+    val md5Size = md5String.length
+    
+    val md5Bytes: MutableList<Byte> = mutableListOf<Byte>()
+    
+    md5Bytes.add(md5Size.toByte())
+    
+    for (i in 1..md5Size) {
+      md5Bytes.add(md5String.get(i - 1).toByte())
+    }
+    
+    val md5Buffer = md5Bytes.toByteArray()
+    val md5Packet: DatagramPacket = DatagramPacket(md5Buffer, md5Buffer.size, serverIP, serverPort)
+    dgramSocket.send(md5Packet)
+    // dgramSocket.receive(dgramPacket)
+
+
+    fileInputStream.close()
   } catch (t: Throwable) {
     println(t)
   }
@@ -94,8 +142,8 @@ fun main() {
   print("Informe o nome do arquivo que você deseja fazer upload: ")
   val filename: String = readLine()!!
 
-  var dgramSocket: DatagramSocket = DatagramSocket(serverPort)
+  var dgramSocket: DatagramSocket = DatagramSocket()
 
-  uploadFile(dgramSocket, filename, serverIP, serverPort)
+  uploadFile(dgramSocket, filename, InetAddress.getByName(serverIP), serverPort)
 }
 
--- socket-udp/segundo/server/main.kt
+++ socket-udp/segundo/server/main.kt
@@ -1,12 +1,108 @@
 package segundo.server
 
+import java.io.File
+import java.math.BigInteger
+import java.net.DatagramPacket
 import java.net.DatagramSocket
+import java.security.MessageDigest
+import kotlin.math.ceil
 
+/**
+ * Função para criar a hash md5
+ */
+fun String.md5(): String {
+  val md = MessageDigest.getInstance("MD5")
+  return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
+}
+
+/**
+ * Função que recebe o arquivo e salva em um diretório padrão do servidor
+ *
+ * @param dgramSocket: Socket responsável por enviar o arquivo
+ * @param localStorage: Local onde o arquivo será salvo
+ */
 fun receiveFile(dgramSocket: DatagramSocket, localStorage: String = "./.shared") {
+
+  try {
+
+    val readBuffer: ByteArray = ByteArray(1024)
+    val readPacket: DatagramPacket = DatagramPacket(readBuffer, readBuffer.size)
+
+    dgramSocket.receive(readPacket)
+
+    val writeBuffer: ByteArray = "ACK".toByteArray(Charsets.UTF_8)
+    val writePacket: DatagramPacket = DatagramPacket(writeBuffer, writeBuffer.size, readPacket.getSocketAddress())
+
+    dgramSocket.send(writePacket) // ACK
+
+    val receivedData = readPacket.getData()
+
+    val filenameSize = receivedData.get(0).toInt()
+    val filenameBytes: MutableList<Byte> = mutableListOf()
+
+    for (i in 1..filenameSize) {
+      filenameBytes.add(receivedData.get(i))
+    }
+
+    val filename: String = filenameBytes.toByteArray().toString(Charsets.UTF_8)
+
+    val fileSizeInit = filenameSize + 1
+
+    val HEXfile3: Int = receivedData.get(fileSizeInit + 0).toInt() shl 24
+    val HEXfile2: Int = receivedData.get(fileSizeInit + 1).toInt() shl 16
+    val HEXfile1: Int = receivedData.get(fileSizeInit + 2).toInt() shl 8
+    val HEXfile0: Int = receivedData.get(fileSizeInit + 3).toInt() shl 0
+
+    val fileSize = HEXfile3 or HEXfile2 or HEXfile1 or HEXfile0
+
+    val file = File(localStorage + "/" + filename)
+
+    val fileOutputStream = file.outputStream()
+    val fileInputStream = file.inputStream()
+
+    val numberOfPackets: Int = ceil(fileSize / 1024.0).toInt()
+
+    for (i in 1..numberOfPackets) {
+      dgramSocket.receive(readPacket)
+      fileOutputStream.write(readPacket.getData())
+      dgramSocket.send(writePacket) // ACK
+    }
+
+    fileOutputStream.close()
+
+    // val md5Buffer = ByteArray(1024)
+    // val md5Packet = DatagramPacket(md5Buffer, md5Buffer.size)
+
+    dgramSocket.receive(readPacket)
+
+    val received = readPacket.getData()
+    val md5Size = received.get(0).toInt()
+
+    val md5Bytes: MutableList<Byte> = mutableListOf()
+
+    for (i in 1..md5Size) {
+      md5Bytes.add(received.get(i).toByte())
+    }
+
+    dgramSocket.send(writePacket) // ACK
+
+    val fileBytes = ByteArray(fileSize)
+    fileInputStream.read(fileBytes)
+
+    fileInputStream.close()
+  } catch (t: Throwable) {
+    println(t)
+  }
 }
 
 fun main() {
+
   try {
+    val socket: DatagramSocket = DatagramSocket(7890)
+
+    Thread {
+      receiveFile(socket)
+    }.start()
   } catch (t: Throwable) {
     println(t)
   }
